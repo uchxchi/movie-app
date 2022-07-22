f800bf64c3857a083d8ffb2f800a4d7af68c293f
Uchechi Nwabia <nwabiaesther@gmail.com>
2022-07-22 23:46:50 +0100

    uchxchi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [movie, setMovie] = useState([])
+
+  useEffect(() => {
+    axios.get('https://uchxchi-movies.herokuapp.com/api/movies/')
+    .then(res => {
+      setMovie(res.data)
+
+    })
+    .catch(err => {
+
+    })
+
+  }, [])
+  if(!movie){
+    return(
+      <div>loading...</div>
+    ) 
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      
+      <header>
+        <h1>UCHXCHI'S MOVIE REVIEWS</h1>
       </header>
+
+      <div className='reviewFilter'>
+        <button className='button'>Add new review</button>
+        <input 
+        name='filter'
+        type="text"
+        placeholder='filter'
+         />
+      </div>
+
+      <div className='card-container'>
+        {
+        movie.map((movie) =>{
+          return (
+            <div className='card'>
+            <h2>{movie.title}</h2>
+            <img 
+            className='img'
+            src={movie.image_url}
+             alt="movie" 
+             />    
     </div>
-  );
-}
+
+          )
+          
+        })
+      }
+      </div>
+
+      
+
+      
+    </div>
+    
+  )
+  }
 
 export default App;
