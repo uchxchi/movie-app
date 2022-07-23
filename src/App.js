461f698566d05d01f86ed0772e2bfd737fd4ad31
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2022-07-23 07:17:29 +0100

    chore: project structure

--- src/App.js
+++ src/App.js
@@ -1,68 +1,8 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import './App.css';
+import React from 'react'
+import { Movies } from './pages/Movies'
 
 function App() {
-  const [movie, setMovie] = useState([])
+  return <Movies />
+}
 
-  useEffect(() => {
-    axios.get('https://uchxchi-movies.herokuapp.com/api/movies/')
-    .then(res => {
-      setMovie(res.data)
-
-    })
-    .catch(err => {
-
-    })
-
-  }, [])
-  if(!movie){
-    return(
-      <div>loading...</div>
-    ) 
-  }
- 
-  return (
-    <div className='container'>
-      
-      <header>
-        <h1>UCHXCHI'S MOVIE REVIEWS</h1>
-      </header>
-
-      <div className='reviewFilter'>
-        <button className='button'>Add new review</button>
-        <input 
-        name='filter'
-        type="text"
-        placeholder='filter'
-         />
-      </div>
-
-      <div className='card-container'>
-        {
-        movie.map((movie) =>{
-          return (
-            <div className='card'>
-            <h2>{movie.title}</h2>
-            <img 
-            className='img'
-            src={movie.image_url}
-             alt="movie" 
-             />    
-    </div>
-
-          )
-          
-        })
-      }
-      </div>
-
-      
-
-      
-    </div>
-    
-  )
-  }
-
-export default App;
+export default App
---
+++ src/pages/Movies.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './movies.css'
+import { Card } from '../components/card'
+
+export const Movies = () => {
+  const [movie, setMovie] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://uchxchi-movies.herokuapp.com/api/movies/')
+      .then((res) => {
+        setMovie(res.data)
+      })
+      .catch((err) => {})
+  }, [])
+  if (!movie) {
+    return <div>loading...</div>
+  }
+  return (
+    <div className='container'>
+      <header>
+        <h1>UCHXCHI'S MOVIE REVIEWS</h1>
+      </header>
+
+      <div className='reviewFilter'>
+        <button className='button'>Add new review</button>
+        <input name='filter' type='text' placeholder='filter' />
+      </div>
+
+      <div className='card-container'>
+        {movie.map((movie) => {
+          return <Card movie={movie} />
+        })}
+      </div>
+    </div>
+  )
+}
