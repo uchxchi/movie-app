579e0d7ab963fb3b14fbf0fbc7a67a1a6ba27905
Uchechi Nwabia <nwabiaesther@gmail.com>
2022-07-24 12:59:15 +0100

    height-adjustment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react'
 import { Movies } from './pages/Movies'
+import { Route, Routes} from 'react-router-dom'
+import SingleMovie from './pages/singleMovie'
+
 
 function App() {
-  return <Movies />
+  return (
+    <div>
+       <div>
+        <Routes>
+          <Route 
+          path='/movie/:id'
+          element={<SingleMovie />}
+          />
+
+          <Route 
+          path='/'
+          element={<Movies />}
+          />
+            
+        </Routes>
+     
+    </div>
+    </div>
+   
+  
+  )
 }
 
 export default App
---
+++ src/pages/singleMovie.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import { Route, Link, Switch} from 'react-router-dom'
+
+export default function SingleMovie(){
+    return(
+        <div></div>
+    )
+}
