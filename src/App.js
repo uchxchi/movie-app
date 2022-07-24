2fb372ba6cf44b7c056de38bd8c5f74c55a1830f
Uchechi Nwabia <nwabiaesther@gmail.com>
2022-07-24 13:21:33 +0100

    card-changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
        <div>
         <Routes>
           <Route 
-          path='/movie/:id'
+          path='/movie/:movieId'
           element={<SingleMovie />}
           />
 
--- src/pages/singleMovie.js
+++ src/pages/singleMovie.js
@@ -1,8 +1,21 @@
-import React from "react";
-// import { Route, Link, Switch} from 'react-router-dom'
+import React, { useEffect} from "react";
+import axios from "axios";
+import { useParams } from 'react-router-dom'
 
 export default function SingleMovie(){
+
+    useEffect(() => {
+        axios.get('https://uchxchi-movies.herokuapp.com/api/movies/:movieId')
+        .then(res => {
+
+        })
+        .catch(err => {
+
+        })
+    })
     return(
-        <div></div>
+        <div>
+
+        </div>
     )
 }
