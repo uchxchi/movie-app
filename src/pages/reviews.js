4fa1ab850e660bd1377b288ff669bfdbf241476c
Uchechi Nwabia <nwabiaesther@gmail.com>
2022-07-25 17:01:30 +0100

    form-skeleton

--- src/pages/reviews.js
+++ src/pages/reviews.js
@@ -3,7 +3,42 @@ import React from "react";
 export default function MovieReview (){
     return(
         <div>
-            <h2>fish</h2>
+           <form>
+
+            <div>
+                 <label for='title'>Title</label>
+                     <input
+                       name="title"
+                       type='text'
+                       id="title"
+                       placeholder="enter title"
+                  />
+            </div>
+
+            <div>
+                <label for='image-link'>Image Link</label>
+                <input
+                name="image-link" 
+                type="url"
+                id="image-link" 
+                placeholder="enter image link"
+                />
+            </div>
+
+            <div>
+                <label for='review'>Review</label>
+                <textarea
+                name="review"
+                rows='5'
+                id="review"
+                placeholder="write your review"
+                />
+            </div>
+
+            <button>ADD REVIEW</button>
+
+           
+           </form>
         </div>
     )
 }
