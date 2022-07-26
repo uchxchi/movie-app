1654e62fd974bf908c78696fcf491bf50fede3e1
Uchechi Nwabia <nwabiaesther@gmail.com>
2022-07-26 13:23:52 +0100

    form-ish

--- src/pages/reviews.js
+++ src/pages/reviews.js
@@ -1,37 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import './reviews.css'
 
-export default function MovieReview (){
+export default function MovieReview (props){
+    const {
+        formValue,
+        submit,
+        change,
+        formErrors,
+        setFormErrors,
+    } = props
+
+    const onChange = (evt) => {
+        const {name, value} = evt.target
+        change(name, value)
+    }
+    const onSubmit = (evt) => {
+        evt.preventDefault()
+        submit()
+    }
+   
     return(
         <div>
-           <form>
+            <h3>CREATE NEW REVIEW</h3>
+           <form onSubmit={onSubmit}>
 
             <div>
-                 <label for='title'>Title</label>
+                <p>{formErrors.title}</p>
+                <p>{formErrors.image_url}</p>
+                <p>{formErrors.review}</p>
+            </div>
+
+            <div className="formInput">
+                 <label htmlFor='title'>Title</label>
                      <input
                        name="title"
+                       value={formValue.title}
                        type='text'
                        id="title"
                        placeholder="enter title"
+                       onChange={onChange}
                   />
             </div>
 
-            <div>
-                <label for='image-link'>Image Link</label>
+            <div className="imgLink">
+                <label htmlFor='imageLink'>Image Link</label>
                 <input
-                name="image-link" 
+                name="image_url" 
+                value={formValue.image_url}
                 type="url"
-                id="image-link" 
-                placeholder="enter image link"
+                id="imageLink" 
+                placeholder="enter image url"
+                onChange={onChange}
                 />
             </div>
 
-            <div>
-                <label for='review'>Review</label>
+            <div className="review">
+                <label htmlFor='review'>Review</label>
                 <textarea
                 name="review"
+                value={formValue.review}
                 rows='5'
                 id="review"
                 placeholder="write your review"
+                onChange={onChange}
                 />
             </div>
 
